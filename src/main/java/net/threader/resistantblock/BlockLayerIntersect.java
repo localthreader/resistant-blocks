d27bcadb3e6f2f9e0baed5e4fe9836b856b59d16
threader <40972803+localthreader@users.noreply.github.com>
2021-09-04 14:07:15 -0300

    Fixing intersector logic

--- src/main/java/net/threader/resistantblock/BlockLayerIntersect.java
+++ src/main/java/net/threader/resistantblock/BlockLayerIntersect.java
@@ -12,19 +12,17 @@ public class BlockLayerIntersect {
 
     private Location[][][] intersect() {
         Location[][][] intersected = new Location[radius*2+1][radius*2+1][radius*2+1];
-        int index = 0;
-        for(int i = -radius; i<radius + 1; i++) {
+        for(int i = 0; i<radius + 1; i++) {
             Location[][] layer = new Location[radius*2+1][radius*2+1];
-            for(int j = -radius; j<radius + 1; j++) {
-                for(int k = -radius; k<radius + 1; k++) {
-                    int x = source.getBlockX() + j;
-                    int y = source.getBlockY() + i;
-                    int z = source.getBlockZ() + k;
+            for(int j = 0; j<radius + 1; j++) {
+                for(int k = 0; k<radius + 1; k++) {
+                    int x = source.getBlockX() + (j - radius);
+                    int y = source.getBlockY() + (i - radius);
+                    int z = source.getBlockZ() + (k - radius);
                     layer[j][k] = new Location(source.getWorld(), x, y, z);
                 }
             }
-            intersected[index] = layer;
-            index++;
+            intersected[i] = layer;
         }
         return intersected;
     }
